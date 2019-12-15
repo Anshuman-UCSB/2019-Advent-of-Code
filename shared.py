d75782aae3f17c077e9d7ec9a078aa497c3961d2
Anshuman <anshuman@ucsb.edu>
2019-12-15 00:51:38 -0800

    Day 5 part 2 solved

--- shared.py
+++ shared.py
@@ -1,11 +1,12 @@
 
 
-def intCode(inp):
+def intCode(inp, debug = 0):
     list = inp.copy()
     index = 0
     while(list[index]!=99):
         #print("block {}:".format(int(index/4+1)))
-        #print("    {}, {}, {}, {}".format(list[index],list[index+1],list[index+2],list[index+3]))
+        if debug > 1:
+            print("    {}, {}, {}, {}".format(list[index],list[index+1],list[index+2],list[index+3]))
 
         instruction = list[index]
         temp = instruction
@@ -16,6 +17,10 @@ def intCode(inp):
             classifier.append(temp%10)
             temp = int(temp/10)
         #print("opcode is {}".format(classifier))
+
+        if debug > 0:
+            print("    List: {}, instruction pointer: {}".format(list,index))
+
 
         if(classifier[0] == 1):
             sum = 0
@@ -54,8 +59,73 @@ def intCode(inp):
             else:
                 output = list[list[index+1]]
             print("Output: {}".format(output))
-            if(output!=0):
+            if(output!=0 and debug>1):
                 print("    {}, {}, {}, {}".format(list[index],list[index+1],list[index+2],list[index+3]))
             index += 2
-        #print(list)
+        elif(classifier[0] == 5):
+            if(classifier[1] == 1):
+                if(list[index+1]!=0):
+                    if(classifier[2] == 1):
+                        index = list[index+2]
+                    else:
+                        index = list[list[index+2]]
+                else:
+                    index += 3
+            else:
+                if(list[list[index+1]]!=0):
+                    if(classifier[2] == 1):
+                        index = list[index+2]
+                    else:
+                        index = list[list[index+2]]
+                else:
+                    index += 3
+
+        elif(classifier[0] == 6):
+            if(classifier[1] == 1):
+                if(list[index+1]==0):
+                    if(classifier[2] == 1):
+                        index = list[index+2]
+                    else:
+                        index = list[list[index+2]]
+                else:
+                    index += 3
+            else:
+                if(list[list[index+1]]==0):
+                    if(classifier[2] == 1):
+                        index = list[index+2]
+                    else:
+                        index = list[list[index+2]]
+                else:
+                    index += 3
+
+        elif(classifier[0] == 7):
+            param = []
+            for i in range(2):
+                if(classifier[i+1] == 1):
+                    param.append(list[index+i+1])
+                else:
+                    param.append(list[list[index+i+1]])
+            param.append(list[index+3])
+
+            if(param[0]<param[1]):
+                list[param[2]]=1
+            else:
+                list[param[2]]=0
+            index += 4
+
+        elif(classifier[0] == 8):
+            param = []
+            for i in range(2):
+                if(classifier[i+1] == 1):
+                    param.append(list[index+i+1])
+                else:
+                    param.append(list[list[index+i+1]])
+            param.append(list[index+3])
+            if debug>0:
+                print("        Executing 8, with param {}".format(param))
+            if(param[0]==param[1]):
+                list[param[2]]=1
+            else:
+                list[param[2]]=0
+            index += 4
     return list
